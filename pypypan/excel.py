16742ab958dd88876d7d00fbec0d1006f3e1b89a
Ate te Voortwis <atv@allseas.com>
2022-01-26 13:15:42 +0100

    Fixes linting issues

--- pypypan/excel.py
+++ pypypan/excel.py
@@ -1,6 +1,4 @@
 from pathlib import Path
-from unittest import expectedFailure
-from xml.dom import NotFoundErr
 import pandas as pd
 from typing import List
 from dataclasses import dataclass
@@ -15,44 +13,49 @@ class CommonsItem:
 
 def read_pattypan_input(filename: Path) -> List[CommonsItem]:
     """
-    Reads an Excel file following the Pattypan format (https://commons.wikimedia.org/wiki/Commons:Pattypan/Simple_manual)
+    Reads an Excel file following the Pattypan format:
+        (https://commons.wikimedia.org/wiki/Commons:Pattypan/Simple_manual)
     This should have two worksheets:
-    - 'Template', containing a single cell A1 that holds the template to use to generate the description for the Commons page
-    - 'Data', containing rows with all items to upload. There are two mandatory columns:
+    - 'Template', containing a single cell A1 that holds the template used
+      to generate the description for the Commons page
+    - 'Data', containing rows with all items to upload.
+      There are two mandatory columns:
         - name: the name (title) of the item
         - path: the path to the file to upload
-      All other columns are optional and free to choose, but they should match a placeholder in the beforementioned template.
+      All other columns are optional and free to choose,
+      but they should match a placeholder in the beforementioned template.
       E.g., a column 'creator' will be used to fill the ${creator} placeholder in the template
 
-    The functions returns a list of CommonsItem objects, which hold a title, a description and a path to the file to upload.
+    The functions returns a list of CommonsItem objects,
+    which hold a title, a description and a path to the file to upload.
     """
     if not filename.exists():
         raise FileNotFoundError("File {filename} does not exist")
 
     try:
         data = pd.read_excel(filename, sheet_name='Data')
-    except:
-        raise NotFoundErr(f"Sheet 'Data' not found in {filename}")
+    except ValueError:
+        raise ValueError(f"Sheet 'Data' not found in {filename}")
 
     try:
         tpl = pd.read_excel(filename, sheet_name='Template')
-    except:
-        raise NotFoundErr(f"Sheet 'Template' not found in {filename}")
-        
+    except ValueError:
+        raise ValueError(f"Sheet 'Template' not found in {filename}")
+
     try:
         tpl = tpl.columns[0]
-    except:
-        raise NotFoundErr(f"Cell A1 not found in {filename}")
+    except ValueError:
+        raise ValueError(f"Cell A1 not found in {filename}")
 
     allowed_missing_columns = ['name', 'path']
     for col in data.columns:
-        if not f"${{{col}}}" in tpl and not col in allowed_missing_columns:
-            raise NotFoundErr(f"Column {col} not used in template")
+        if f"${{{col}}}" not in tpl and col not in allowed_missing_columns:
+            raise ValueError(f"Column {col} not used in template")
 
     items = []
     for i, row in data.iterrows():
         if not Path(row.path).exists:
-            raise FileNotFoundError( "File {filename} does not exist")
+            raise FileNotFoundError("File {filename} does not exist")
         tpl_values = {}
         for col in data.columns:
             if col in allowed_missing_columns:
--- test/test_excel.py
+++ test/test_excel.py
@@ -3,9 +3,12 @@ import pytest
 import pathlib
 BASE_PATH = pathlib.Path(__file__).parent
 
+
 def test_read_valid_excel():
     data = excel.read_pattypan_input(BASE_PATH / "./data/data_test.xls")
+    assert len(data) == 2
+
 
 def test_read_not_existing_excel():
     with pytest.raises(FileNotFoundError):
-        data = excel.read_pattypan_input(BASE_PATH / "./data/data_testabc.xls")
+        excel.read_pattypan_input(BASE_PATH / "./data/data_testabc.xls")
