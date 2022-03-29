5673190724b90795a3bf3b8d400888fb4e137bb5
Ate te Voortwis <atetevoortwis@gmail.com>
2022-03-29 20:48:35 +0200

    First implementation of hashing to check if image exists

--- pypypan/upload.py
+++ pypypan/upload.py
@@ -2,6 +2,8 @@ import logging
 import pywikibot
 from pathlib import Path
 from pypypan.excel import read_pattypan_input, CommonsItem
+import requests
+import hashlib
 
 site = pywikibot.Site('commons:commons')
 test_site = pywikibot.Site('commons:test')
@@ -11,11 +13,41 @@ def sanitize_title(title: str) -> str:
     # Remove double apostrophes: https://commons.wikimedia.org/wiki/MediaWiki:Titleblacklist-custom-double-apostrophe
     return title.replace("''", "'")
 
+def phash_image(filename: Path) -> str:
+    from PIL import Image
+    import imagehash
+    return str(imagehash.phash(Image.open(filename)))
+    
+def sha1hash_image(filename: Path) -> str:
+    BLOCK_SIZE = 65536 # The size of each read from the file
+
+    file_hash = hashlib.sha1() 
+    with open(filename, 'rb') as f: 
+        fb = f.read(BLOCK_SIZE) 
+        while len(fb) > 0: 
+            file_hash.update(fb) 
+            fb = f.read(BLOCK_SIZE)
+    return str(file_hash.hexdigest())
+
+def check_if_image_exists(filename: Path):
+    # First check the SHA1 hash
+    hash = sha1hash_image(filename)
+    o = requests.get("https://commons.wikimedia.org//w/api.php?action=query&format=json&list=allimages&aisha1={}".format(hash)).json()
+
+    try:
+        if len(o['query']['allimages']) > 0:
+            return True,o['query']['allimages'][0]['name']
+        else:
+            return False,None
+    except:
+        return False,None
+
 
 def upload_image(item: CommonsItem, site: pywikibot.Site, update_existing: bool = False, dry_run: bool = False) -> False:
     imagepage = pywikibot.FilePage(site, sanitize_title(item.title))
     imagepage.text = item.description
     try:
+        # First, check if an imagepage with this title exists
         if imagepage.exists():
             if update_existing:
                 logging.info(f"Updating description of {item.path.name}")
@@ -30,6 +62,10 @@ def upload_image(item: CommonsItem, site: pywikibot.Site, update_existing: bool 
             if dry_run:
                 return True
             else:
+                # check if the image (hash based) already exists
+                exists, name = check_if_image_exists(item.path)
+                if exists:
+                    logging.info("Skipping upload of {} since a file with the same SHA1 hash exists: {}".format(item.title,name))
                 return site.upload(filepage=imagepage, source_filename=item.path, ignore_warnings=True)
 
     except Exception as e:
